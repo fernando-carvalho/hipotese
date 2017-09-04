b1300df11fa910ef8e0763690365d1b90f8fb69d
fernando-carvalho <fernandocarvalho3101@gmail.com>
2017-09-04 09:32:09 -0300

    Update gaussian.py

--- gaussian.py
+++ gaussian.py
@@ -10,3 +10,20 @@ print gauss
 Sample run:
 0.176032663382
 '''
+from math import e, sqrt, pi
+import numpy as np
+
+m = 0.52
+s = 0.5
+
+vetor = []
+
+for x in np.arange(-3.0, 3.0, 0.05):
+	vetor.append(1/(sqrt(2*pi)*s)*e**(-0.5*(float(x-m)/s)**2))
+
+print vetor
+
+import matplotlib.pyplot as plt
+
+plt.scatter(np.arange(-3.0, 3.0, 0.05), vetor)
+plt.show()
